9f90f99e1ad555b52b52c3e77278e36f20df8ed1
havvand <90390212+havvand@users.noreply.github.com>
2022-11-04 13:40:18 +0100

    AccountDataBase 2nd

--- src/AccountDataBase.java
+++ src/AccountDataBase.java
@@ -4,34 +4,60 @@ import java.util.Scanner;
 
 public class AccountDataBase
 {
-    private String userName;
-    private String passWord;
+    Scanner scan = new Scanner(System.in);
     ArrayList<Account> userAccounts = new ArrayList<>();
+    private String inputOne;
+    private String inputTwo;
 
     public AccountDataBase()
     {
 
     }
 
-    void addUser(Account account)
+    // This method has to register and add new users.
+    protected void addUser(Account account)
     {
         userAccounts.add(account);
     }
 
-    void userAuthentication(Account account)
+    protected void userAuthentication(Account account)
     {
-
-        Scanner scan = new Scanner(System.in);
         System.out.println("Type username and password for " + account);
-        String inputOne = scan.nextLine();
-        String inputTwo = scan.nextLine();
-
+        inputOne = scan.nextLine();
+        inputTwo = scan.nextLine();
         if (!Objects.equals(inputOne, account.getUserName()) || !Objects.equals(inputTwo, account.getPassWord()))
         {
-            System.out.println("Wrong username or password");
-            userAuthentication(account);
+            // CALL userExist
+            System.out.println(account.getUserName());
+            System.out.println(userAccounts);
+            userExists(userAccounts,account);
+            System.out.println("After userEx");
+
+            // SOUT Would you like to create a new user? - INPUT?
+            // TODO: 04-11-2022  Make call for login.newUser();
         } else
-        System.out.println("Logging in.");
+        System.out.println("CALL MAIN-MENU");
+    }
+
+    // This method checks if the user exists.
+    protected boolean userExists(ArrayList<Account> userAccounts, Account account)
+    {
+
+        for (Account a: userAccounts)
+        {
+            a = userAccounts.get(1);
+            if(Objects.equals(a.getUserName(), account.getUserName()))
+            {
+                System.out.println("Wrong username");
+                userAuthentication(account);
+                return true;
+            }
+            System.out.println("FUCK" + a.getUserName() + account.getUserName());
+            return false;
+        }
+        //if (!Object.equals(account.getPassWord() || !Objects.equals(account.getPassWord()))
+        System.out.println("Would you like to create a new user?");
+        return false;
     }
 
     public String toString()
