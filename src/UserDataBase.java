f3bd5293fcc8fdb717d7c0946bf30c2d54912eb7
havvand <90390212+havvand@users.noreply.github.com>
2022-11-04 08:51:02 +0100

    UDB Class gittest2

--- src/UserDataBase.java
+++ src/UserDataBase.java
@@ -1,2 +1,21 @@
-public class UserDataBase {
+public class UserDataBase
+{
+    private String userName;
+    private String passWord;
+
+    public UserDataBase(String userName, String passWord)
+    {
+        this.userName = userName;
+        this.passWord = passWord;
+    }
+
+    public String getUserName()
+    {
+        return this.userName;
+    }
+
+    public String getPassWord()
+    {
+        return this.passWord;
+    }
 }
