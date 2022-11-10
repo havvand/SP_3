92a041fbc3bebba89f212725e352c055b114b24c
havvand <90390212+havvand@users.noreply.github.com>
2022-11-10 11:00:53 +0100

    Login moved to AccountDataBase. AccountDataBase made prettier and quit added!!

---
+++ src/ADTest.java
@@ -0,0 +1,18 @@
+/*import org.junit.jupiter.api.Test;
+
+public class ADTest {
+    @Test
+    public void testUserE(){
+        AccountDataBase test = new AccountDataBase();
+        Account one = new Account("Benny", "Benny1");
+        Account two = new Account("Holger", "Holger1");
+        Account three = new Account("Jens", "Jens1");
+        test.addUser(one);
+        test.addUser(two);
+        test.addUser(three);
+
+    }
+
+
+
+}*/
